5ac3698f7ef5677cfffd66a63d7cc8ff4cd32e47
z4y4nP <zayan.funtime@gmail.com>
2024-12-14 19:13:53 +0000

    calc

---
+++ index.js
@@ -0,0 +1,14 @@
+var num1 = 12;
+var num2 = 6;
+var add = num1 +num2;
+var sub = num1 - num2;
+var mul = num1 * num2;
+var div = num1 / num2;
+document.getElementById("+").innerHTML =
+"Numbers added together are; " + add;
+document.getElementById("-").innerHTML =
+"Numbers subtracted are; " + sub;
+document.getElementById("x").innerHTML =
+"Numbers multiplied together are; " + mul;
+document.getElementById("/").innerHTML =
+"Numbers divided are; " + div;
